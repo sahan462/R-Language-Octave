24d65520f34bc2bfae417c003f1907dbbb99fd24
damitha sahan <sahanchandrasena462@gmail.com>
2024-01-08 08:47:55 +0530

    prac04 q1 done

--- practical-04/21000298.R
+++ practical-04/21000298.R
@@ -1,3 +1,8 @@
-values <- c(580000, 450000, 330000, 380000, 280000, 310000)
-labels <- c("colombo", "Kandy", "Galle", "Anuradhapura", "Trincomalee", "Jaffna")
+values_1 <- c(580000, 450000, 330000, 380000, 280000, 310000)
+labels_1 <- c("colombo", "Kandy", "Galle", "Anuradhapura", "Trincomalee", "Jaffna")
 pie(values, labels, main="City pie chart", col=rainbow(length(values)))
+
+values_2 <- c(580000, 450000, 330000, 380000, 280000, 310000)
+labels_2 <- c("colombo", "Kandy", "Galle", "Anuradhapura", "Trincomalee", "Jaffna")
+piepercent <- round(100*values_2/sum(values_2), 1)
+pie(values_2, labels_2 = piepercent, main = "City pie chart", col = rainbow(length(values_2)))
