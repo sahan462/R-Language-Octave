05f3cedbecf60844762cd38418e5a37cbb6faaff
damitha sahan <sahanchandrasena462@gmail.com>
2024-01-08 09:13:15 +0530

    prac04 q2 done

--- practical-04/21000298.R
+++ practical-04/21000298.R
@@ -1,3 +1,5 @@
+#pie charts
+
 population_1 <- c(580000, 450000, 330000, 380000, 280000, 310000)
 city_1 <- c("colombo", "Kandy", "Galle", "Anuradhapura", "Trincomalee", "Jaffna")
 pie(population_1, city_1, main="City pie chart", col=rainbow(length(values)))
@@ -6,3 +8,10 @@ population_2 <- c(580000, 450000, 330000, 380000, 280000, 310000)
 city_2 <- c("colombo", "Kandy", "Galle", "Anuradhapura", "Trincomalee", "Jaffna")
 piepercent <- round(100*values_2/sum(population_2), 1)
 pie(population_2, city_2 = piepercent, main = "City pie chart", col = rainbow(length(values_2)))
+
+
+#bar charts
+
+year <- c(2015, 2016, 2017, 2018, 2019)
+yearly_income <- c(1320000, 1500000, 2002000, 1980000, 1520000)
+barplot(yearly_income, names.arg=year, xlab="year", ylab="yearly income",col="red",main="Income",border="blue")
